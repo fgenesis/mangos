515255f0dacea467b36a96be41d9430803257151
fgenesis <false.genesis@googlemail.com>
2009-03-21 01:20:23 +0100

    workaround: fix orb of deception + polymorph exploit

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2904,6 +2904,9 @@ void Aura::HandleAuraTransform(bool apply, bool Real)
     }
     else
     {
+        // FG: workaround, fix extreme regeneration bug (orb of deception + polymorph)
+        m_target->setTransForm(0);
+
         Unit::AuraList const& otherTransforms = m_target->GetAurasByType(SPELL_AURA_TRANSFORM);
         if(otherTransforms.empty())
         {
