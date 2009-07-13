a29c66c825b93ad356b0a258bab62d61aab1ed5c
fgenesis <false.genesis@googlemail.com>
2009-07-14 01:35:56 +0200

    Revert "tempfixed spell power healing bonus /w adjustable multi in conf"
    This reverts commit 57012c2bf6f1e7aea60ed4f5ab756123a2ddc84b.

--- src/game/StatSystem.cpp
+++ src/game/StatSystem.cpp
@@ -23,8 +23,6 @@
 #include "SharedDefines.h"
 #include "SpellAuras.h"
 
-#include "World.h"
-
 /*#######################################
 ########                         ########
 ########   PLAYERS STAT SYSTEM   ########
@@ -113,7 +111,7 @@ void Player::UpdateSpellDamageAndHealingBonus()
     // Magic damage modifiers implemented in Unit::SpellDamageBonus
     // This information for client side use only
     // Get healing bonus for all schools
-    SetStatInt32Value(PLAYER_FIELD_MOD_HEALING_DONE_POS, (int32)(SpellBaseHealingBonus(SPELL_SCHOOL_MASK_ALL) / sWorld.getRate(MULTI_SPELL_BASE_HEALING_BONUS))); // FG: HACKFIX: divide by multi to fix client display
+    SetStatInt32Value(PLAYER_FIELD_MOD_HEALING_DONE_POS, SpellBaseHealingBonus(SPELL_SCHOOL_MASK_ALL));
     // Get damage bonus for all schools
     for(int i = SPELL_SCHOOL_HOLY; i < MAX_SPELL_SCHOOL; ++i)
         SetStatInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_POS+i, SpellBaseDamageBonus(SpellSchoolMask(1 << i)));
