98256d6e7410aa1130181399980cdcf9f96b76ab
fgenesis <false.genesis@googlemail.com>
2009-07-23 16:03:44 +0200

    applied "Triggered Spell Mechanics and Miss Chance" patch by nos4r2zod
    forum link: http://getmangos.com/community/showthread.php?t=9123

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5297,7 +5297,7 @@ void Spell::EffectScriptEffect(uint32 effIndex)
                         //}
                     }
                     if (spellId)
-                        m_caster->CastCustomSpell(unitTarget, spellId, &basePoint, 0, 0, false);
+                        m_caster->CastCustomSpell(unitTarget, spellId, &basePoint, 0, 0, true);
                     return;
                 }
                 default:
