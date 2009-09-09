85af8d657e92fdfbee911a8d64266567d4e687d3
fgenesis <false.genesis@googlemail.com>
2009-09-09 19:19:11 +0200

    [p228] Fix: Don't apply level penality to spells with no max level.
    Conflicts:
    
    	src/game/Unit.cpp

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -2371,15 +2371,21 @@ uint32 Unit::CalculateDamage (WeaponAttackType attType, bool normalized, bool ad
 
 float Unit::CalculateLevelPenalty(SpellEntry const* spellProto) const
 {
-    if(spellProto->spellLevel <= 0)
+    if (spellProto->spellLevel <= 0)
         return 1.0f;
 
     float LvlPenalty = 0.0f;
 
-    if(spellProto->spellLevel < 20)
-        LvlPenalty = 20.0f - spellProto->spellLevel * 3.75f;
-    float LvlFactor = (float(spellProto->spellLevel) + 6.0f) / float(getLevel());
-    if(LvlFactor > 1.0f || spellProto->maxLevel == 0)
+    if (spellProto->baseLevel < 20)
+        LvlPenalty = 1.0f - (20.0f - spellProto->baseLevel) * 0.0375f;
+    else
+       LvlPenalty = 1.0f;
+
+    if (LvlPenalty < 0)
+        LvlPenalty = 0;
+
+    float LvlFactor = (float(spellProto->maxLevel) + 6.0f) / float(getLevel());
+    if (LvlFactor > 1.0f || spellProto->maxLevel == 0)
         LvlFactor = 1.0f;
 
     return (100.0f - LvlPenalty) * LvlFactor / 100.0f;
