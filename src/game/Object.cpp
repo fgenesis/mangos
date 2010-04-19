9264c2c952bfcfcdc10d21b41f79f7cc777010e1
fgenesis <false.genesis@googlemail.com>
2010-04-19 17:55:08 +0200

    some crash fixes (taken from: http://github.com/kicho/test/commit/626494a1059ff4c1609a6c7b487e163b3328155e)
    Conflicts:
    
    	src/game/MovementHandler.cpp
    	src/game/Object.cpp

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -823,7 +823,12 @@ void Object::_SetCreateBits(UpdateMask *updateMask, Player* /*target*/) const
 
 void Object::SetInt32Value( uint16 index, int32 value )
 {
-    ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    //ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    if (index >= m_valuesCount)
+    {
+        PrintIndexError(index, true);
+        return;
+    }
 
     if(m_int32Values[ index ] != value)
     {
@@ -842,7 +847,12 @@ void Object::SetInt32Value( uint16 index, int32 value )
 
 void Object::SetUInt32Value( uint16 index, uint32 value )
 {
-    ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    //ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    if (index >= m_valuesCount)
+    {
+        PrintIndexError(index, true);
+        return;
+    }
 
     if(m_uint32Values[ index ] != value)
     {
@@ -861,7 +871,9 @@ void Object::SetUInt32Value( uint16 index, uint32 value )
 
 void Object::SetUInt64Value( uint16 index, const uint64 &value )
 {
-    if( index + 1 > m_valuesCount) { PrintIndexError( index, true ); return; }
+    //ASSERT( index + 1 < m_valuesCount || PrintIndexError( index, true ) );
+	if( index + 1 > m_valuesCount) { PrintIndexError( index, true ); return; }
+
     if(*((uint64*)&(m_uint32Values[ index ])) != value)
     {
         m_uint32Values[ index ] = *((uint32*)&value);
@@ -880,7 +892,12 @@ void Object::SetUInt64Value( uint16 index, const uint64 &value )
 
 void Object::SetFloatValue( uint16 index, float value )
 {
-    ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    //ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    if (index >= m_valuesCount)
+    {
+        PrintIndexError(index, true);
+        return;
+    }
 
     if(m_floatValues[ index ] != value)
     {
@@ -899,7 +916,12 @@ void Object::SetFloatValue( uint16 index, float value )
 
 void Object::SetByteValue( uint16 index, uint8 offset, uint8 value )
 {
-    ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    //ASSERT( index < m_valuesCount || PrintIndexError( index, true ) )
+    if (index >= m_valuesCount)
+    {
+        PrintIndexError(index, true);
+        return;
+    }
 
     if(offset > 4)
     {
@@ -925,7 +947,12 @@ void Object::SetByteValue( uint16 index, uint8 offset, uint8 value )
 
 void Object::SetUInt16Value( uint16 index, uint8 offset, uint16 value )
 {
-    ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    //ASSERT( index < m_valuesCount || PrintIndexError( index, true ) );
+    if (index >= m_valuesCount)
+    {
+        PrintIndexError(index, true);
+        return;
+    }
 
     if(offset > 2)
     {
