a322ddfe4c941019b12d085b8c67740d7f460c2b
zerg <Sergey.Chelombitko@gmail.com>
2010-08-19 01:00:35 +0600

    [192] Fix Penance exploit Thx to original author(s)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2125,6 +2125,20 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 }
                 break;
             }
+            case SPELLFAMILY_PRIEST:
+            {
+                // Penance
+                if (GetSpellProto()->SpellIconID == 225 || GetSpellProto()->SpellIconID == 2818)
+                {
+                    Unit* caster = GetCaster();
+                    if (!caster || caster->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    ((Player*)caster)->SetSelection(target->GetGUID());
+                    return;
+                }
+                break;
+            }
             case SPELLFAMILY_SHAMAN:
             {
                 switch(GetId())
