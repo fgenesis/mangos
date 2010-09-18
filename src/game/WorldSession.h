8da804ac37e89b9213159da1fa7b10394a380640
fgenesis <false.genesis@googlemail.com>
2010-09-18 23:57:53 +0200

    [249] Implement CMSG_PET_STOP_ATTACK
    Conflicts:
    
    	src/game/PetHandler.cpp
    	src/game/WorldSession.h

--- src/game/WorldSession.h
+++ src/game/WorldSession.h
@@ -656,6 +656,7 @@ class MANGOS_DLL_SPEC WorldSession
         //Pet
         void HandlePetAction( WorldPacket & recv_data );
         void HandlePetNameQueryOpcode( WorldPacket & recv_data );
+        void HandlePetStopAttackOpcode(WorldPacket & recv_data);
         void HandlePetSetAction( WorldPacket & recv_data );
         void HandlePetAbandon( WorldPacket & recv_data );
         void HandlePetRename( WorldPacket & recv_data );
