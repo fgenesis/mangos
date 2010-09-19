4e17b0c70d5fd1a13cf636edb6d2533707f6e437
fgenesis <false.genesis@googlemail.com>
2010-09-19 04:05:37 +0200

    forgot something (addition to ec9dd02159835009ddbbdfb2f323c3b8874dd984)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7604,7 +7604,7 @@ uint32 Unit::MeleeDamageBonusTaken(Unit *pCaster, uint32 pdamage,WeaponAttackTyp
     uint32 mechanicMask     = spellProto ? GetAllSpellMechanicMask(spellProto) : 0;
 
     // Shred also have bonus as MECHANIC_BLEED damages
-    if (spellProto && spellProto->SpellFamilyName==SPELLFAMILY_DRUID && spellProto->SpellFamilyFlags & UI64LIT(0x00008000))
+    if (spellProto && spellProto->SpellFamilyName==SPELLFAMILY_DRUID && spellProto->SpellFamilyFlags & UI64LIT(0x00008800))
         mechanicMask |= (1 << (MECHANIC_BLEED-1));
 
 
