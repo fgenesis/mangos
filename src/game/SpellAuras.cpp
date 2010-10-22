5ca4971b1813e5c23796b3d3a64fa34f23a05d02
fgenesis <false.genesis@googlemail.com>
2010-10-22 21:04:53 +0200

    Revert "do not send aura remove/add for auras in AURA_REMOVE_BY_STACK mode, and skip few other actions"
    This reverts commit d5d0638acdb17d70430b5f305ba79eb0bcf32013.
    This fixes problems with auras stacking with themselves multiple times.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8446,7 +8446,7 @@ void SpellAuraHolder::_RemoveSpellAuraHolder()
     SetAuraLevel(0);
     SetVisibleAura(true);
 
-    if (m_removeMode != AURA_REMOVE_BY_DELETE && m_removeMode != AURA_REMOVE_BY_STACK)
+    if (m_removeMode != AURA_REMOVE_BY_DELETE)
     {
         SendAuraUpdate(true);
 
