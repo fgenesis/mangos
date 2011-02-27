d6529584ebecf2f86a9f2ad87f9418893a748d84
fgenesis <false.genesis@googlemail.com>
2011-02-27 02:49:58 +0100

    Nitro boosts drop bg flag, by przemratajczak
    https://github.com/przemratajczak/mangos/commit/416646174a9bceec205f701688e3515dc29d8c08

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2003,6 +2003,16 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     else                                    // Knocked Up   - backfire 5%
                         m_caster->CastSpell(m_caster, 46014, true, m_CastItem);
 
+                    if (m_caster->GetTypeId() == TYPEID_PLAYER)
+                    {
+                        Player* pPlayer = ((Player*)m_caster);
+                        // Nitro Boosts - drop BG flag
+                        if(pPlayer->InBattleGround() && (pPlayer->HasAura(23335) || pPlayer->HasAura(23333) || pPlayer->HasAura(34976)))
+                            if(BattleGround *bg = pPlayer->GetBattleGround())
+                                bg->EventPlayerDroppedFlag(pPlayer);
+                    }
+
+
                     return;
                 }
                 case 55818:                                 // Hurl Boulder
