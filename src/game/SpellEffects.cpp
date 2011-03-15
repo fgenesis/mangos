892c1264ddf8880b89313e001cafb3dbb08188f5
fgenesis <false.genesis@googlemail.com>
2011-03-15 16:17:10 +0100

    fixed freezing arrow, really places a trap now. hopefully no bad side effects.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3375,6 +3375,10 @@ void Spell::EffectTriggerMissileSpell(SpellEffectIndex effect_idx)
     if (m_CastItem)
         DEBUG_FILTER_LOG(LOG_FILTER_SPELL_CAST, "WORLD: cast Item spellId - %i", spellInfo->Id);
 
+    // FG: triggered missile spells on cooldown is wrong imho. fixes freezing arrow at least.
+    if(m_caster->GetTypeId() == TYPEID_PLAYER)
+        ((Player*)m_caster)->RemoveSpellCooldown(triggered_spell_id);
+
     m_caster->CastSpell(m_targets.m_destX, m_targets.m_destY, m_targets.m_destZ, spellInfo, true, m_CastItem, 0, m_originalCasterGUID);
 }
 
