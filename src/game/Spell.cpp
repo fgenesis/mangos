fgenesis/mangos

commit ce6eb5d4049441b911dcd5df8dbc6bc6834fcf5d

abort cast if target goes invisible or out of LOS

src/game/Spell.cpp
@@ -3460,6 +3460,10 @@ void Spell::update(uint32 difftime)
     {
         case SPELL_STATE_PREPARING:
         {
+            // FG: check if target is still visible (stealth + LOS check)
+            if(m_caster->GetTypeId() == TYPEID_PLAYER && !IsTargetVisibleAndInLOS())
+                cancel();
+
             if(m_timer)
             {
                 if(difftime >= m_timer)
@@ -3473,6 +3477,14 @@ void Spell::update(uint32 difftime)
         } break;
         case SPELL_STATE_CASTING:
         {
+            // FG: check if target is still visible (stealth + LOS check)
+            // FIXME: this is not correct, channeling should continue, just drop a tick:
+            /* "For channeled spells, however, the mob may not be attacked one second or another,
+               or perhaps the effect that the spell gives (such as [Mind Flay]) may not proc,
+               but the next tick where there is no obstruction during that span, it will continue." */
+            if(m_caster->GetTypeId() == TYPEID_PLAYER && !IsTargetVisibleAndInLOS())
+                cancel();
+
             if(m_timer > 0)
             {
                 if( m_caster->GetTypeId() == TYPEID_PLAYER )
@@ -7449,4 +7461,12 @@ void Spell::CancelGlobalCooldown()
         m_caster->GetCharmInfo()->GetGlobalCooldownMgr().CancelGlobalCooldown(m_spellInfo);
     else if (m_caster->GetTypeId() == TYPEID_PLAYER)
         ((Player*)m_caster)->GetGlobalCooldownMgr().CancelGlobalCooldown(m_spellInfo);
+}
+
+bool Spell::IsTargetVisibleAndInLOS(void) const
+{
+    if(Unit *target = m_targets.getUnitTarget())
+        return target->isVisibleForOrDetect(m_caster, m_caster, true) && m_caster->IsWithinLOSInMap(target);
+
+    return false;
 }

src/game/Spell.h
@@ -639,6 +639,11 @@ class Spell
         // we can't store original aura link to prevent access to deleted auras
         // and in same time need aura data and after aura deleting.
         SpellEntry const* m_triggeredByAuraSpell;
+
+
+    // FG: custom stuff
+    public:
+        bool IsTargetVisibleAndInLOS(void) const;
 };
 
 enum ReplenishType

commit b7454125e1f9f9f60a02cd09cc1e272e9bc9414b

improved spell LOS handling added yesterday
this fixes problems introduced in ce6eb5d4049441b911dcd5df8dbc6bc6834fcf5d

src/game/Spell.cpp
@@ -3460,8 +3460,8 @@ void Spell::update(uint32 difftime)
     {
         case SPELL_STATE_PREPARING:
         {
-            // FG: check if target is still visible (stealth + LOS check)
-            if(m_caster->GetTypeId() == TYPEID_PLAYER && !IsTargetVisibleAndInLOS())
+            // FG: check if target is still visible (stealth + LOS check) -- spells with cast time
+            if(m_caster->GetTypeId() == TYPEID_PLAYER && GetCastTime() && !IsTargetVisibleAndInLOS())
                 cancel();
 
             if(m_timer)
@@ -3477,13 +3477,32 @@ void Spell::update(uint32 difftime)
         } break;
         case SPELL_STATE_CASTING:
         {
-            // FG: check if target is still visible (stealth + LOS check)
-            // FIXME: this is not correct, channeling should continue, just drop a tick:
+            // FG: check if target is still visible (stealth + LOS check) -- channeled spells
+            // FIXME: this is not correct, channeling should continue, just drop a tick.
             /* "For channeled spells, however, the mob may not be attacked one second or another,
                or perhaps the effect that the spell gives (such as [Mind Flay]) may not proc,
                but the next tick where there is no obstruction during that span, it will continue." */
-            if(m_caster->GetTypeId() == TYPEID_PLAYER && !IsTargetVisibleAndInLOS())
-                cancel();
+            if(m_caster->GetTypeId() == TYPEID_PLAYER && IsChanneledSpell(m_spellInfo))
+            {
+                // HACK: LOS detection, unknown if there is a spell attribute for this
+                bool checkLOS = true;
+                for(uint32 idx = 0; idx < MAX_EFFECT_INDEX; ++idx)
+                {
+                    if(m_spellInfo->Effect[idx] == SPELL_EFFECT_APPLY_AURA)
+                    {
+                        switch(m_spellInfo->EffectApplyAuraName[idx])
+                        {
+                            case SPELL_AURA_BIND_SIGHT:
+                            case SPELL_AURA_MOD_POSSESS:
+                            case SPELL_AURA_MOD_STALKED:
+                                checkLOS = false;
+                                break;
+                        }
+                    }
+                }
+                if(checkLOS && !IsTargetVisibleAndInLOS())
+                    cancel();
+            }
 
             if(m_timer > 0)
             {